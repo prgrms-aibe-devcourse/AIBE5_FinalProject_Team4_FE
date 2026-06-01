3a20a067ad5727b6a54f16d741bd7bf00584c439
devken65 <effortcoders@gmail.com>
2026-06-01 14:17:11 +0900

    fix: ClosetTab.tsx 일부 비활성화, index.ts 에러 성공처리수정

--- src/components/ClosetTab.tsx
+++ src/components/ClosetTab.tsx
@@ -1,5 +1,7 @@
+/* Todo: data를 실제로 쓸 준비가 되면 다시 추가
 import Spinner from "@/components/common/Spinner";
 import useApi from "@/hooks/useApi";
+ */
 import React, { useState, useMemo } from "react";
 import { 
   Heart, 
@@ -31,7 +33,7 @@ export default function ClosetTab({
   toggleFavorite,
   moveToOwnedCloset
 }: ClosetTabProps) {
-  const {data, error, loading} = useApi('/api/v1/wardrobes/users/1');
+  // const {data, error, loading} = useApi('/api/v1/wardrobes/users/1'); Todo: data를 실제로 쓸 준비가 되면 다시 추가
   const [closetTab, setClosetTab] = useState<"owned" | "wishlist">("owned");
   const [closetFilter, setClosetFilter] = useState<string>("All");
   const [toastMessage, setToastMessage] = useState<string | null>(null);
@@ -72,8 +74,10 @@ export default function ClosetTab({
     triggerToast(`🛍️ 축하합니다! "${name}" 의상이 최상단 보유 옷장 목록으로 이동했습니다!`);
   };
 
+  /* Todo: data를 실제로 쓸 준비가 되면 다시 추가
   if(loading) return <Spinner />;
   if(error) return <div>{error}</div>;
+   */
 
   return (
     <div className="space-y-6 animate-fade-in font-sans">
