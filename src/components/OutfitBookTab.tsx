95d47ef3ee0b8afd7a5b6f2bfaf01c877028545e
warcat12 <warcat12@gmail.com>
2026-06-24 16:38:30 +0900

    feat/#125 피드 상세 수정

--- src/components/OutfitBookTab.tsx
+++ src/components/OutfitBookTab.tsx
@@ -31,6 +31,7 @@ interface OutfitBookTabProps {
   clothes: Garment[];
   guideTourCompleted: boolean;
   onGuideTourComplete: () => void;
+  refreshSignal?: number;
 }
 
 export default function OutfitBookTab({
@@ -38,6 +39,7 @@ export default function OutfitBookTab({
                                         clothes,
                                         guideTourCompleted,
                                         onGuideTourComplete,
+                                        refreshSignal = 0,
                                       }: OutfitBookTabProps) {
   const [outfits, setOutfits] = useState<OutfitResponse[]>([]);
   const [outfitBookId, setOutfitBookId] = useState<number | null>(null);
@@ -71,7 +73,7 @@ export default function OutfitBookTab({
 
   useEffect(() => {
     loadOutfits();
-  }, [loadOutfits]);
+  }, [loadOutfits, refreshSignal]);
 
   const filteredOutfits = useMemo(() => {
     let list = outfits;
